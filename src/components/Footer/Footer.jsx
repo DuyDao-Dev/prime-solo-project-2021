8748f7f913b07b638ac80bb32bb5716fec65fca8
Duy Dao <jimmydao@gmail.com>
2021-08-10 14:15:20 -0500

    Added material ui to registration page. Link at the bottom does not take user back to login page.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Footer.css';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -7,7 +9,16 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Coding Daonamics</footer>;
+  return(
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Daonamic Coding
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 export default Footer;
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -17,18 +17,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Daonamic Coding
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -147,8 +136,7 @@ function LoginPage() {
                 variant="body2"
                 onClick={() => {
                   history.push("/registration");
-                }}
-              >
+                }}>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
@@ -156,7 +144,6 @@ function LoginPage() {
         </form>
       </div>
       <Box mt={8}>
-        <Copyright />
       </Box>
     </Container>
   );
