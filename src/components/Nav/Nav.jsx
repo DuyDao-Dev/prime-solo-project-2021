4316e2445fd83ecd22db946d042061297e2c20b9
Duy Dao <jimmydao@gmail.com>
2021-08-11 23:43:36 -0500

    Trying to move the LogOutButton component to the nav bar but its breaking.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,173 +1,178 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import {useSelector} from 'react-redux';
+//Material UI Nav Bar
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useDispatch, useSelector } from "react-redux";
+
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function Nav() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   let loginLinkData = {
-    path: '/login',
-    text: 'Login / Register',
+    path: "/login",
+    text: "Login / Register",
   };
 
   if (user.id != null) {
-    loginLinkData.path = '/user';
-    loginLinkData.text = 'Home';
+    loginLinkData.path = "/user";
+    loginLinkData.text = "Home";
+  }
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logOutButton = (props) => {
+    className={props.className}
+      dispatch({ type: 'LOGOUT' })
   }
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">No Thyme</h2>
-      </Link>
-      <div>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
-        <Link className="navLink" to="/search">
-          Search
-        </Link>
-        <Link className="navLink" to="/favorite">
-          Favorite Recipes
-        </Link>
-        <Link className="navLink" to="/shopping">
-          Shopping List
-        </Link>
-        {user.id && (
-          <>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
+    <div className={classes.root}>
+      <AppBar position="relative">
+        <Toolbar>
+          <MenuIcon
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          />
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem
+              className="navLink"
+              to={loginLinkData.path}
+              onClick={handleClose}
+            >
+              {loginLinkData.text}
+            </MenuItem>
+            <MenuItem className="navLink" onClick={history.push("/home")}>
+              Home
+            </MenuItem>
+            <MenuItem className="navLink" onClick={history.push("/search")}>
+              Search
+            </MenuItem>
+            <MenuItem className="navLink" onClick={history.push("/favorite")}>
+              Favorite Recipes
+            </MenuItem>
+            <MenuItem className="navLink" onClick={history.push("/shopping")}>
+              Shopping List
+            </MenuItem>
+            {user.id && (
+              <>
+                <MenuItem className="navLink" onClick={history.push("/search")}>
+                  About
+                </MenuItem>
+                <LogOutButton className="navLink" />
+                {/* <MenuItem className="navLink" 
+            onClick={handleClose}>Logout</MenuItem> */}
+              </>
+            )}
+          </Menu>
+          {/* </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            No Thyme
+          </Typography>
+          <Button color="inherit"
+            onClick={() => {logOutButton(props)}}>Logout</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
 
 export default Nav;
 
-//Material UI Nav Bar
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import MenuIcon from "@material-ui/icons/Menu";
 
-// import Menu from "@material-ui/core/Menu";
-// import MenuItem from "@material-ui/core/MenuItem";
+//Original Link codes
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import LogOutButton from '../LogOutButton/LogOutButton';
+// import './Nav.css';
+// import {useSelector} from 'react-redux';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2)
-//   },
-//   title: {
-//     flexGrow: 1
-//   }
-// }));
+// function Nav() {
+//   const user = useSelector((store) => store.user);
 
-// export default function ButtonAppBar() {
-//   const classes = useStyles();
-//   const [anchorEl, setAnchorEl] = React.useState(null);
+  // let loginLinkData = {
+  //   path: '/login',
+  //   text: 'Login / Register',
+  // };
 
-//   const handleClick = (event) => {
-//     setAnchorEl(event.currentTarget);
-//   };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//   };
+  // if (user.id != null) {
+  //   loginLinkData.path = '/user';
+  //   loginLinkData.text = 'Home';
+  // }
 
 //   return (
-//     <div className={classes.root}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <MenuIcon
-//             aria-controls="simple-menu"
-//             aria-haspopup="true"
-//             onClick={handleClick}
-//           />
-//           <Menu
-//             id="simple-menu"
-//             anchorEl={anchorEl}
-//             keepMounted
-//             open={Boolean(anchorEl)}
-//             onClose={handleClose}
-//           >
-//             <MenuItem onClick={handleClose}>Profile</MenuItem>
-//             <MenuItem onClick={handleClose}>My account</MenuItem>
-//             <MenuItem onClick={handleClose}>Logout</MenuItem>
-//           </Menu>
-//           {/* </IconButton> */}
-//           <Typography variant="h6" className={classes.title}>
-//             No Thyme
-//           </Typography>
-//           <Button color="inherit">Login</Button>
-//         </Toolbar>
-//       </AppBar>
+//     <div className="nav">
+//       <Link to="/home">
+//         <h2 className="nav-title">No Thyme</h2>
+//       </Link>
+//       <div>
+//         <Link className="navLink" to={loginLinkData.path}>
+//           {loginLinkData.text}
+//         </Link>
+//         <Link className="navLink" to="/search">
+//           Search
+//         </Link>
+//         <Link className="navLink" to="/favorite">
+//           Favorite Recipes
+//         </Link>
+//         <Link className="navLink" to="/shopping">
+//           Shopping List
+//         </Link>
+//         {user.id && (
+//           <>
+//             <Link className="navLink" to="/info">
+//               Info Page
+//             </Link>
+//             <LogOutButton className="navLink" />
+//           </>
+//         )}
+
+//         <Link className="navLink" to="/about">
+//           About
+//         </Link>
+// //       </div>
 //     </div>
 //   );
 // }
 
-// <div>
-// <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-
-// </Button>
-// <Menu
-//   id="simple-menu"
-//   anchorEl={anchorEl}
-//   keepMounted
-//   open={Boolean(anchorEl)}
-//   onClose={handleClose}
-// >
-//   <MenuItem onClick={handleClose}>Profile</MenuItem>
-//   <MenuItem onClick={handleClose}>My account</MenuItem>
-//   <MenuItem onClick={handleClose}>Logout</MenuItem>
-// </Menu>
-// </div>
+// export default Nav;
 
 
 
-
-
-  // return (
-  //   <div className={classes.root}>
-  //     <AppBar position="static">
-  //       <Toolbar>
-  //         <MenuIcon
-  //           aria-controls="simple-menu"
-  //           aria-haspopup="true"
-  //           onClick={handleClick}
-  //         />
-  //         <Menu
-  //           id="simple-menu"
-  //           anchorEl={anchorEl}
-  //           keepMounted
-  //           open={Boolean(anchorEl)}
-  //           onClose={handleClose}
-  //         >
-  //           <MenuItem onClick={handleClose}>Profile</MenuItem>
-  //           <MenuItem onClick={handleClose}>My account</MenuItem>
-  //           <MenuItem onClick={handleClose}>Logout</MenuItem>
-  //         </Menu>
-  //         {/* </IconButton> */}
-  //         <Typography variant="h6" className={classes.title}>
-  //           No Thyme
-  //         </Typography>
-  //         <Button color="inherit">Login</Button>
-  //       </Toolbar>
-  //     </AppBar>
-  //   </div>
-  // );
