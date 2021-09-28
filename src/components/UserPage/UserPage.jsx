9a4872b08f60af677ec762c8a6701350e0f01bbb
Duy Dao <jimmydao@gmail.com>
2021-09-28 09:26:00 -0500

    Added some clearer instructions for users and a search button for users to search in case they cant find the nav button

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useHistory } from 'react-router';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
+
   return (
     <div className="container">
       <h2>Welcome to No Thyme, {user.username}!</h2>
@@ -12,12 +15,23 @@ function UserPage() {
       <h3>Where having No Thyme, saves time.</h3>
       <ul>
         <li>
-          With No Thyme you can enter your ingredients you have on hand and
-          search for recipes matching those ingredients.
+          With No Thyme you can enter your ingredients you have on hand,
+          or find recipes with certain ingredients,
+          you will be able to search for recipes matching those ingredients. 
         </li>
+        <br></br>
         <li>Enjoy those recipes? Add them to your favorites! </li>
+        <br></br>
         <li>Missing ingredients? No problem! Check off the missing
           ingredients and add them to your shopping list. 
+        </li>
+        <br></br>
+        <li>
+          Ready to start searching for recipes? Click on the 
+          <button
+            onClick={() => {
+              history.push("/search");
+            }}>Search</button> button.
         </li>
       </ul>
     </div>
